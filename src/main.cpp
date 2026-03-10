2299cc0ec7e049ec8b1247dbc54f76335d4c34d1
theflyoccultist <y.delahaije@gmail.com>
2026-03-10 19:52:55 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,21 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  while (true) {
+  bool running = true;
+
+  while (running) {
+    // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
     std::cout << "$ ";
     std::string command;
     std::getline(std::cin, command);
+
+    if (command == "exit") {
+      running = false;
+    }
+
     std::cerr << command << ": command not found\n";
   }
 }
