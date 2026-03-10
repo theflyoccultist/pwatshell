f2273fab2c154ec3eddcbadf057baee3373e6289
theflyoccultist <y.delahaije@gmail.com>
2026-03-10 19:50:02 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  while (true) {
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  std::string command;
-  std::getline(std::cin, command);
-  std::cerr << command << ": command not found";
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
+    std::cerr << command << ": command not found";
+  }
 }
